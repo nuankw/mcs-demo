863281be1fec9a04122953b9c5d1b6a93a2e4824
g1eb <mail@g1eb.com>
2020-08-25 09:10:47 -0700

    Convert Instructions component into a class based component

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import React from 'react'
 import Button from '@material-ui/core/Button'
 import Accordion from '@material-ui/core/Accordion'
 import AccordionSummary from '@material-ui/core/AccordionSummary'
 import AccordionDetails from '@material-ui/core/AccordionDetails'
 import Typography from '@material-ui/core/Typography'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import { withStyles } from '@material-ui/core/styles'
 
 import DomainInstructions from './DomainInstructions'
 import ScenarioInstructions from './ScenarioInstructions'
 
 
-const useStyles = makeStyles((theme) => ({
+const styles = theme => ({
   root: {
     width: '100%',
   },
@@ -28,32 +28,53 @@ const useStyles = makeStyles((theme) => ({
     color: "#ffffff",
     textTransform: "none",
   }
-}))
+})
 
 
-export default function Instructions() {
-  const classes = useStyles()
+class Instructions extends React.Component {
 
-  const [expanded, setExpanded] = useState(true)
+  constructor(props) {
+    super(props)
 
-  return (
-    <div className={classes.root}>
-      <Accordion expanded={expanded} className={classes.accordion} defaultExpanded>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          onClick={() => setExpanded(!expanded)}
-          aria-controls="panel1a-content"
-          id="instructions">
-          <Typography className={classes.heading}>Instructions</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-            <DomainInstructions />
-            <ScenarioInstructions />
-        </AccordionDetails>
-        <Button className={classes.got_it} variant="contained" style={{'cursor': 'pointer'}} onClick={() => setExpanded(!expanded)}>
-            Got it! Let's proceed.
-          </Button>
-      </Accordion>
-    </div>
-  )
+    this.state = {
+      expanded: true,
+    }
+  }
+
+  toggle() {
+    const { expanded } = this.state
+    this.setState({expanded: !expanded})
+  }
+
+  render() {
+    const { expanded } = this.state
+    const { classes } = this.props
+    return (
+      <div className={classes.root}>
+        <Accordion expanded={expanded} className={classes.accordion} defaultExpanded>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            onClick={this.toggle.bind(this)}
+            aria-controls="panel1a-content"
+            id="instructions">
+            <Typography className={classes.heading}>Instructions</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+              <DomainInstructions />
+              <ScenarioInstructions />
+          </AccordionDetails>
+          <Button
+            className={classes.got_it}
+            variant="contained"
+            style={{'cursor': 'pointer'}}
+            onClick={this.toggle.bind(this)}>
+              Got it! Let's proceed.
+            </Button>
+        </Accordion>
+      </div>
+    )
+  }
 }
+
+
+export default withStyles(styles)(Instructions)
