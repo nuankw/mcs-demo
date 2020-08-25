289cca8b16879b47be85a0ba79c90dbd09d1b880
g1eb <mail@g1eb.com>
2020-08-25 09:13:27 -0700

    Add the initial component for the creation part of the demo

---
+++ src/components/Creation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class Creation extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    )
+  }
+}
+
+
+export default Creation
