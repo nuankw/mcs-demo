63380816a15684010b270b1c6c18a5b17cf50c38
g1eb <mail@g1eb.com>
2020-09-29 18:00:42 -0700

    Add the popover with text showing on hover over the question mark

--- src/components/Whaaaat.js
+++ src/components/Whaaaat.js
@@ -1,28 +1,70 @@
 import React from 'react'
+import Popover from '@material-ui/core/Popover'
+import Typography from '@material-ui/core/Typography'
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline'
-import { withStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 
 
-const styles = theme => ({
+const useStyles = makeStyles((theme) => ({
   icon: {
     position: "relative",
     top: theme.spacing(2),
     right: theme.spacing(1),
     color: 'lightgoldenrodyellow',
-    fontSize: theme.spacing(3),
+    fontSize: theme.spacing(5),
+    cursor: 'pointer',
   },
-})
+  popover: {
+    pointerEvents: 'none',
+  },
+  paper: {
+    padding: theme.spacing(1),
+  },
+}))
 
 
-class Whaaaat extends React.Component {
+export default function Whaaaat() {
+  const classes = useStyles()
+  const [anchorEl, setAnchorEl] = React.useState(null)
 
-  render() {
-    const { classes } = this.props
-    return (
-      <HelpOutlineIcon className={classes.icon} />
-    )
-  }
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget)
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null)
+  };
+
+  const open = Boolean(anchorEl)
+
+  return (
+    <React.Fragment>
+      <HelpOutlineIcon
+        className={classes.icon}
+        aria-owns={open ? 'mouse-over-popover' : undefined}
+        aria-haspopup="true"
+        onMouseEnter={handlePopoverOpen}
+        onMouseLeave={handlePopoverClose} />
+      <Popover
+        id="mouse-over-popover"
+        className={classes.popover}
+        classes={{
+          paper: classes.paper,
+        }}
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus>
+        <Typography>asdfasdfasfdasdfasfdasdf</Typography>
+      </Popover>
+    </React.Fragment>
+  )
 }
-
-
-export default withStyles(styles)(Whaaaat)
