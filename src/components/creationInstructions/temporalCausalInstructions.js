5458f305552038bf6e1d8a6d0c0ac5dcba937b66
Nuan Wen <nuankw@yahoo.com>
2020-10-02 22:29:49 -0700

    instruction skeletons

---
+++ src/components/creationInstructions/temporalCausalInstructions.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
+
+import { Grid } from '@material-ui/core'
+
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    fontSize: '18px',
+  },
+  instructions: {
+    paddingLeft: theme.spacing(3),
+    paddingRight: theme.spacing(4),
+  },
+  head: {
+    fontWeight: 'bold',
+  },
+  headExplanation: {
+    marginTop: theme.spacing(0.2),
+    marginBottom: theme.spacing(0.2),
+  },
+})
+
+
+class TimeCausalInstruction extends React.Component {
+
+  render() {
+    const { classes } = this.props
+    return (
+      <Grid container  className={classes.root}>
+        <Grid item xs={12}>
+          <Typography component={'div'} className={classes.instructions}>
+            <ul>
+              <li className={classes.head}>
+                  Domain and Scenario Recap
+              </li>
+              <ul>
+                <li><b>Time</b>: Knowledge regarding
+                  scheduling activities and their durations.</li>
+                <li><b>Cause & Effect</b>: Answers the “Why” question or predicts what is likely to happen next (effect),
+                  given an event that has occurred (cause).</li>
+              </ul>
+
+              <li className={classes.head}>
+                  Examples
+              </li>
+              <ul>
+                <li>
+                  <b><i>[True/False]</i></b> Example 1-1 <br/>
+                  <b><i>[False/True]</i></b> Example 1-2
+                </li>
+                <li>
+                  <b><i>[True/False]</i></b> Example 2-1 <br/>
+                  <b><i>[False/True]</i></b> Example 2-2
+                </li>
+              </ul>
+
+              <li className={classes.head}>
+                  Tips and Tricks
+              </li>
+
+              <ul>
+                <li className={classes.headExplanation}>
+                  To break the system as well as generate complementary samples,
+                  we can modify the sentence with:
+                </li>
+                <ul>
+                  <li>Negation</li>
+                  <li>Swap</li>
+                  <li>Plausibility-Qualifiers (e.g. sometimes, likely, rarely)</li>
+                  <li>Contrasting-Conjuncts (e.g. unless, despite)</li>
+                  <li>Distractors</li>
+                </ul>
+
+                <li>
+                  Tip 2
+                </li>
+
+                <li>
+                  Tip 3
+                </li>
+              </ul>
+
+            </ul>
+          </Typography>
+
+        </Grid>
+      </Grid>
+    )
+  }
+}
+
+
+export default withStyles(styles)(TimeCausalInstruction)
